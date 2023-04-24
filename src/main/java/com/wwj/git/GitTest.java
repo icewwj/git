91e13e7d4a69123e41be37e3d441406f0be65e58
ice <ice@wwj.com>
2023-04-24 10:03:01 +0800

    Second commit

--- src/main/java/com/wwj/git/GitTest.java
+++ src/main/java/com/wwj/git/GitTest.java
@@ -3,5 +3,9 @@ package com.wwj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
